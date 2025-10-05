2826728e3eefe347356fb03670d29b0d1c2eac2a
Sean Winslow <sean.winslow28@gmail.com>
2025-10-05 09:12:58 -0700

    fix: correct CSS imports (design & motion tokens)

--- Sean-Winslow-Portfolio/src/main.jsx
+++ Sean-Winslow-Portfolio/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-impoimport './styles/tokens.css';
+import './styles/tokens.css';
 import './styles/tokens-motion.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
